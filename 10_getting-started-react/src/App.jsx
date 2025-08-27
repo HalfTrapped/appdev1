9165b978a6562a25c383044b7662dd7f3b166bf5
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-08-27 15:37:16 +0800

    Activity 10: Add React About Me project

---
+++ 10_getting-started-react/src/App.jsx
@@ -0,0 +1,16 @@
+export default function App(){
+  return (
+    <div>
+      <h1>About Me!</h1>
+      <h2>Hello my name is Aaron Matthew Tulalian</h2>
+      <h2>I am studying BS Information Technology in 3rd Year</h2>
+      <h2>I am studying BS Information Technology in 3rd Year</h2>
+      <h1>Fun fact</h1>
+      <h3> about me is that I can get absorbed at my work and forgetting to eat or pee </h3>
+      <h1>Why I Want to Learn Git & GitHub</h1>
+      <p>
+        I want to learn Git and GitHub because it's necessary tool if I want to succeed and work as a part of developer team by being an SQA, PM, etc. If I can learn how to do git commands and navigate Github like its the back of hand then I would be a good asset to the company.
+      </p>
+    </div>
+  );
+}
