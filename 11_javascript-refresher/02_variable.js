2dc23604c9cdcc649fb200ac4b3a1bdc6985e0ae
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-09-06 09:57:23 +0800

    add variables activity

---
+++ 11_javascript-refresher/02_variable.js
@@ -0,0 +1,17 @@
+const name = "Choi"
+const age = 20
+const isStudent = true
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+
+console.log(typeof(name))
+
+console.log(1+1)
+console.log(2-1)
+console.log(1*1)
+console.log(1/1)
+
+console.log('5' === 5)
+console.log('5' == 5)
