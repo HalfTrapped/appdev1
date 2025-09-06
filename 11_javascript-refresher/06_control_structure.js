a602f8325a556d5aa01d230090633398cfe45246
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-09-06 10:09:36 +0800

    add control structures activity

---
+++ 11_javascript-refresher/06_control_structure.js
@@ -0,0 +1,28 @@
+const grade = 60;
+
+if(grade >= 90){
+    console.log(`Your grade is A`)
+}
+
+else if(grade >= 80){
+    console.log(`Your grade is B`)
+}
+
+else if(grade >= 70){
+    console.log(`Your grade is C`)
+}
+
+else{
+    console.log(`Your grade is F`)
+}
+
+for(let i = 0; i < 5; i++){
+    console.log(`${i}`)
+}
+
+j = 0
+
+while(j < 3){
+    console.log(`Hello`)
+    j++
+}
