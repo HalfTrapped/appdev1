c7b958fae1839c8b9d0bd2b8a317cc2b5258ffd3
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-09-06 10:34:26 +0800

    add essential features activity

---
+++ 11_javascript-refresher/08_essential_feature.js
@@ -0,0 +1,7 @@
+hobbies = ["painting", "drawing", "gaming"]
+
+hobbiesUpdate = [...hobbies, "programming", "eating"]
+
+const showHobbies = hobbiesUpdate.map((hobbiesUpdate) => `The hobby is ${hobbiesUpdate} `)
+
+console.log(showHobbies)
