c7e09d0f8a8400e644a2ad922ae13cffc6b28921
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-09-06 11:10:05 +0800

    add let and const activity

---
+++ 11_javascript-refresher/10_let_const.js
@@ -0,0 +1,12 @@
+
+var nameVar = `Matthew`
+var ageVar = 20
+
+console.log(`My name is ${nameVar} and I am ${ageVar} years old.`)
+
+let city  = "Manila"
+city  = "Apalit"
+console.log(city)
+
+const country = "Philippines"
+console.log(country)
