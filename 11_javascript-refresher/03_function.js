3c7b3baadc5f1b881f164c4699dd200f60203dc8
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-09-06 10:00:00 +0800

    add functions activity

---
+++ 11_javascript-refresher/03_function.js
@@ -0,0 +1,16 @@
+function greet(name) {
+    return console.log(name)
+}
+
+greet("choi")
+
+const square = (number) => { console.log(number * number)}
+
+square(7)
+
+function calculator(a,b) {
+    console.log( a + b )
+    console.log ( a * b )
+}
+
+calculator( 3 , 5)
