5227dd72c5dce4621932fd8b20458f29deb21918
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-09-17 16:32:38 +0800

    Done with conditional rendering

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,26 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-const user = {
-  name: 'Hedy Lamarr',
-  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
-  imageSize: 90,
-};
+let content;
+
+if (True) {
+  content = <AdminPanel />;
+} else {
+  content = <LoginForm />;
+}
+
+function AdminPanel() {
+  return <h1>Welcome, Admin!</h1>;
+}
+
+function LoginForm() {
+  return <h1>Please log in to continue.</h1>;
+}
 
 function App(){
    return (
-    <>
-      <h1>{user.name}</h1>
-      <img
-        className="avatar"
-        src={user.imageUrl}
-        alt={'Photo of ' + user.name}
-        style={{
-          width: user.imageSize,
-          height: user.imageSize
-        }}
-      />
-    </>
+    <div>
+      {content}
+    </div>
     )
  
 }
