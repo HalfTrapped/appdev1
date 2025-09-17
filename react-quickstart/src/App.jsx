237d59b3dfa9cb8c99fd0027e486bd250acc998f
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-09-17 13:09:15 +0800

    Done writing markup JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,19 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function ItoButton() {
- return (
-  <button> Click me</button>
- )
-}
-
 function App(){
-  return (
-    <div className="App">
+   return (
+    <>
       <h1>This is a button</h1>
-      <ItoButton />
-    </div>
-  )
+      <p>This is a markup<br /> wow! space!</p>
+    </>
+    )
+ 
 }
 
 export default App
