b5f3fc523379506652e6599a8b42af899095ac85
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-09-17 15:42:55 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,10 +3,25 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
 function App(){
    return (
     <>
-      <h1 style={{fontstyle: 'comic sans', color: 'blue'}}>Hello World!</h1>
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
     )
  
