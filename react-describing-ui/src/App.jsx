6e575aa8463fddec21f03829d117f84a2bc66c91
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-10-25 09:54:05 +0800

    Done with how to use curly braces with JSX toa ccess JavaScript functionality from your components

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -6,14 +6,7 @@ export default function App() {
   return (
     <>
     <section>
-      <h1>Hedy Lamarr's Todos</h1>
       <Profile />
-      <ul>
-        <li>Invent new traffic lights</li>
-        <li>Rehearse a movie scene</li>
-        <li>Improve spectrum technology</li>
-      </ul>
-
     </section>
     </>
   )
--- react-describing-ui/src/component/Profile.jsx
+++ react-describing-ui/src/component/Profile.jsx
@@ -1,12 +1,27 @@
 export default function Profile() {
+    const person = {
+        name: 'Gregorio Y. Zara',
+        theme: {
+            backgroundColor: 'black',
+            color: 'pink'
+        }
+      };
     return (
-    <>
-      <img 
-        src="https://i.imgur.com/yXOvdOSs.jpg"
-        alt="Hedy Lamarr"
-        className="photo"
-      />
-    </>
+        <>
+            <div style={person.theme}>
+                <h1>{person.name}'s Todos</h1>
+                <img
+                    className="avatar"
+                    src="https://i.imgur.com/7vQD0fPs.jpg"
+                    alt="Gregorio Y. Zara"
+                />
+                <ul>
+                    <li>Improve the videophone</li>
+                    <li>Prepare aeronautics lectures</li>
+                    <li>Work on the alcohol-fuelled engine</li>
+                </ul>
+            </div>
+        </>
   )
 }
   
