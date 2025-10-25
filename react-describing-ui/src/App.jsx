9a752cbc3f30a9cbfb7c07e040d33f62180c96b2
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-10-25 10:46:11 +0800

    Done with how to configure components with props

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,13 +1,37 @@
 import './App.css'
-import Profile from './component/Profile'
+import {getImageUrl} from './component/utils'
 
 export default function App() {
-
   return (
-    <>
-    <section>
-      <Profile />
-    </section>
-    </>
+    <Card>
+      <Avatar
+        size={100}
+        person={{
+          name: 'Katsuko Saruhashi',
+          imageId: 'YfeOqp2'
+        }}
+      />
+    </Card>
   )
 }
+
+function Avatar({ person, size }) {
+  return (
+    <img
+      className="avatar"
+      src={getImageUrl(person)}
+      alt={person.name}
+      width={size}
+      height={size}
+    />
+  );
+}
+
+function Card({ children }) {
+  return (
+    <div className="card">
+      {children}
+    </div>
+  );
+}
+
