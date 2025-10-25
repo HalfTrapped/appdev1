cce2c62d3f42475dddf22a90c4fe96447d68e6ad
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-10-25 09:24:38 +0800

    Done with how to write your first react component

---
+++ react-describing-ui/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img 
+      src="https://i.imgur.com/MK3eW3As.jpg"
+      alt="Katherine Johnson"
+      />
+    </>
+  )
+}
+
+export default function Gallery() {
+  return(
+    <section>
+    <h1>Amazing Scientist</h1>
+    <App />
+    <App />
+    <App />
+    </section>
+  );
+}
