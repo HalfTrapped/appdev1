4153efaf89d42f26dbd3ac2b7c0d0c712892a71b
7078-cj <7078ceejay@gmail.com>
2025-11-22 14:24:54 +0800

    Done with Passing strings with quotes

---
+++ react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  const avatar = "https://i.imgur.com/7vQD0fPs.jpg"
+  const description = "Gregorio Y. Zara"
+  return (
+    <>
+      <img
+        className = "avatar"
+        src = {avatar}
+        alt = {description}
+      />
+    </>
+  )
+}
+
+export default App
