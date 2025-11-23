c57564236dec6d3646e570081aa2b1d61680b54d
Aaron Matthew Tulalian <asianwater1@gmail.com>
2025-11-23 19:18:07 +0800

    Done with fetching user data exercise

---
+++ react-useeffect-simple-api-fetch/src/UsersFetch.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function UsersFetch() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      // Fetch data when the component first loads
+      fetch("https://jsonplaceholder.typicode.com/users?_limit=5")
+        .then(response => response.json())
+        .then(data => {
+          setUsers(data);
+          console.log(data);
+          setLoading(false);
+        })
+        .catch(error => {
+          console.error("Error fetching user info:", error);
+          setLoading(false);
+        });
+      }, []);
+
+  return (
+    <>
+      <h2>Users Info</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            <div><span>Name: </span>{user.name}</div>
+            <div><span>Email: </span>{user.email}</div>
+            <br/>
+          </li>
+        ))}
+      </ul>
+
+    </>
+  )
+}
+
+export default UsersFetch
